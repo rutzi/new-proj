8115b69bfba2f080234ca57a71f49c8f523bad92
Yurii Pylyponiuk <rutzi112@gmail.com>
2019-09-26 21:41:48 +0300

    new-slider-

--- main.js
+++ main.js
@@ -6,4 +6,14 @@ $(document).ready(function(){
 	slidesToShow: 1,
 	adaptiveHeight: true
   });
+
+
+  $('.sliders').slick({
+	dots: false,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true,
+	adaptiveWidth: true
+  });
 });
