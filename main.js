085c87741ec7bb3d07e650c209b8a30cab4a2889
Yurii Pylyponiuk <rutzi112@gmail.com>
2019-10-13 20:24:47 +0300

    acordeon-completed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+
   $('.slider1').slick({
 	dots: false,
 	infinite: true,
@@ -16,4 +16,26 @@ $(document).ready(function(){
 	adaptiveHeight: true,
 	adaptiveWidth: true
   });
-});
+
+
+
+  let newsBtn = document.querySelectorAll('.news-title');
+  let news = document.querySelectorAll('.news-text-block');
+  for(var i = 0; i < newsBtn.length; i++){
+  	newsBtn[i].addEventListener('click', accarBtn);
+  };
+
+  function accarBtn (){
+
+  	if(!(this.classList.contains('active'))){
+  		
+  		this.classList.add('active')
+	  }else if(this.classList.contains('active')){
+	  	this.classList.remove('active')
+	 		
+	 		//Якщо хочеш видаляти всі класи актів при кожному кліку
+	  	// for(var i = 0; i < newsBtn.length; i++){
+	  	// 		newsBtn[i].classList.remove('active');
+	  	// }
+	  }
+	};
